db6de06d94cdbf4e772f57921e338dee15582356
David Michael <davidnagib101@gmail.com>
2024-07-17 14:25:25 +0300

    Adjusted shape and dimensions of item count widget

--- lib/item_count_widget.dart
+++ lib/item_count_widget.dart
@@ -20,12 +20,13 @@ class _ItemCountWidgetState extends State<ItemCountWidget> {
       elevation: 7.5,
       shadowColor: Colors.black,
       color: const Color(0xFF08090A),
-      margin: const EdgeInsets.fromLTRB(15.0, 18.0, 15.0, 0.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      margin: const EdgeInsets.fromLTRB(12.0, 15.0, 12.0, 0.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-            padding: const EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 40.0),
+            padding: const EdgeInsets.fromLTRB(20.0, 40.0, 10.0, 40.0),
             width: 170.0,
             child: Text(
               widget.item.name,
@@ -37,43 +38,59 @@ class _ItemCountWidgetState extends State<ItemCountWidget> {
               ),
             ),
           ),
-          FloatingActionButton( // THE SUBTRACT BUTTON
-            onPressed: () => {
-              setState(() => {
-                if(widget.item.removeOne()) widget.receipt.removeFromReceipt(widget.item)
-              })
-            },
-            backgroundColor: const Color(0xFF730C0C),
-            child: const Text(
-              '-',
-              style: TextStyle(
-                fontSize: 35.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ),
-          Text(
-            '${widget.item.count}',
-            style: TextStyle(
-              color: getCountColor(widget.item.count),
-              fontSize: 43.0,
-              fontWeight: FontWeight.w700
-            ),
-          ),
-          FloatingActionButton( // THE ADD BUTTON
-            onPressed: () => {
-              setState(() => {
-                if(widget.item.addOne()) widget.receipt.addToReceipt(widget.item)
-              })
-            },
-            backgroundColor: const Color(0xFF38482C),
-            child: const Text(
-              '+',
-              style: TextStyle(
-                  fontSize: 35.0,
-                  color: Colors.white
-              ),
+          Container(
+            width: 175.0,
+            padding: const EdgeInsets.only(right: 17.5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 53.0,
+                  child: FloatingActionButton( // THE SUBTRACT BUTTON
+                    onPressed: () => {
+                      setState(() => {
+                        if(widget.item.removeOne()) widget.receipt.removeFromReceipt(widget.item)
+                      })
+                    },
+                    backgroundColor: const Color(0xFF730C0C),
+                    child: const Text(
+                      '-',
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  '${widget.item.count}',
+                  style: TextStyle(
+                    color: getCountColor(widget.item.count),
+                    fontFamily: 'Saira',
+                    fontSize: 43.0,
+                    fontWeight: FontWeight.w800
+                  ),
+                ),
+                SizedBox(
+                  width: 53.0,
+                  child: FloatingActionButton( // THE ADD BUTTON
+                    onPressed: () => {
+                      setState(() => {
+                        if(widget.item.addOne()) widget.receipt.addToReceipt(widget.item)
+                      })
+                    },
+                    backgroundColor: const Color(0xFF38482C),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                          fontSize: 35.0,
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
